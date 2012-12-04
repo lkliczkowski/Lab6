61cf625c2ef5268bade1734353a7a7030156b733
Łukasz Kliczkowski <lkliczkowski@p109-1.labpk.inf.ug.edu.pl>
2012-12-04 09:19:05 +0100

    1 rozwiązanie.

---
+++ lab6.sh
@@ -0,0 +1,20 @@
+# Zad.1
+touch plik.txt
+
+egrep "." plik.txt
+
+# Zad.2
+
+egrep ^[0-9] pl*
+
+#Zad.3
+
+egrep "^........R" plik.txt
+
+# Zad.4
+
+egrep bash /etc/passwd | wc -l
+
+# Zad.5
+
+egrep "(X|D|C|M|V|L|I)" plik.txt
